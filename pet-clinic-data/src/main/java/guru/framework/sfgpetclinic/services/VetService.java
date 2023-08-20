3f3b3e253c1ed0f62f5b0d73450edc38f24ac21b
sparshgahlot1 <sparshgahlot0398@gmail.com>
2023-08-20 12:03:06 +0530

    Refactor Services to Common Interface

--- pet-clinic-data/src/main/java/guru/framework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/guru/framework/sfgpetclinic/services/VetService.java
@@ -4,10 +4,5 @@ import guru.framework.sfgpetclinic.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
